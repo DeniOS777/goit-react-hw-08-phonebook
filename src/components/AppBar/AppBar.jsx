a0907f78e076bfbde8424cb29343329e09c397df
DeniOS777 <Denios777@gmail.com>
2022-10-18 23:40:28 +0300

    added some components

---
+++ src/components/AppBar/AppBar.jsx
@@ -0,0 +1,5 @@
+import React from 'react';
+
+export const AppBar = () => {
+  return <header>AppBar</header>;
+};
---
+++ src/components/UserMenu/UserMenu.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export const UserMenu = () => {
+  return (
+    <div>
+      <img src="/" alt="avatar" />
+      <p>userName</p>
+      <button type="button">Logout</button>
+    </div>
+  );
+};
