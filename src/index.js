07c23490ae25fdee9b667cda7d9d2194c2972936
DeniOS777 <Denios777@gmail.com>
2022-10-20 23:25:03 +0300

    added a persist

--- src/index.js
+++ src/index.js
@@ -1,22 +1,25 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { store } from 'redux/store';
+import { BrowserRouter } from 'react-router-dom';
+import { persistor, store } from 'redux/store';
 import { Provider } from 'react-redux';
+import { PersistGate } from 'redux-persist/integration/react';
 import { ThemeProvider } from 'styled-components';
 import { ToastContainer } from 'react-toastify';
-import 'react-toastify/dist/ReactToastify.css';
 import App from 'components/App';
-import { GlobalStyle } from './components/GlobalStyle';
-import { theme } from './constants';
-import { BrowserRouter } from 'react-router-dom';
+import { GlobalStyle } from 'components/GlobalStyle';
+import { theme } from './constants/theme';
+import 'react-toastify/dist/ReactToastify.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <ThemeProvider theme={theme}>
       <Provider store={store}>
-        <BrowserRouter basename="/goit-react-hw-08-phonebook">
-          <App />
-        </BrowserRouter>
+        <PersistGate loading={null} persistor={persistor}>
+          <BrowserRouter basename="/goit-react-hw-08-phonebook">
+            <App />
+          </BrowserRouter>
+        </PersistGate>
       </Provider>
       <GlobalStyle />
       <ToastContainer autoClose={2000} />
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,11 +1,38 @@
 import { configureStore } from '@reduxjs/toolkit';
+import {
+  persistStore,
+  persistReducer,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+} from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
 
+import { authSlice } from 'redux/auth/authSlice';
 import contactsSlice from 'redux/contacts/contactsSlice';
 import { filterSlice } from 'redux/filter/filterSlice';
 
+const persistConfig = {
+  key: 'auth',
+  storage,
+  whitelist: ['token'],
+};
+
 export const store = configureStore({
   reducer: {
+    auth: persistReducer(persistConfig, authSlice.reducer),
     contacts: contactsSlice,
     filter: filterSlice.reducer,
   },
+  middleware: getDefaultMiddleware =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    }),
 });
+
+export const persistor = persistStore(store);
