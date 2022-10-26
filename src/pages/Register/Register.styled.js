5f52585c88f8598ee798eb90b92f2b0b7d9a0315
DeniOS777 <Denios777@gmail.com>
2022-10-26 16:29:34 +0300

    changed styles to Login and Register pages

--- src/pages/Register/Register.styled.js
+++ src/pages/Register/Register.styled.js
@@ -1,14 +1,17 @@
 import styled from 'styled-components';
 
 export const Container = styled.div`
-  display: flex;
+  /* display: flex;
   flex-direction: column;
-  justify-content: center;
+  justify-content: center; */
+  padding-top: 64px;
+  padding-bottom: 64px;
   padding-left: 16px;
   padding-right: 16px;
   margin: 0 auto;
   max-width: 450px;
   height: calc(100vh - 127px);
+  overflow-y: auto;
 
   *:last-child {
     margin-bottom: 0;
