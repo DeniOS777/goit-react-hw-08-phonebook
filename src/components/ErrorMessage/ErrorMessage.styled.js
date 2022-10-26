9733e0cece8689ed420edd7b9d25c6073c7b8e9f
DeniOS777 <Denios777@gmail.com>
2022-10-27 00:10:15 +0300

    fix errorMessage width

--- src/components/ErrorMessage/ErrorMessage.styled.js
+++ src/components/ErrorMessage/ErrorMessage.styled.js
@@ -1,7 +1,9 @@
 import styled from 'styled-components';
 
 export const Error = styled.p`
+  margin: 0 auto;
   padding: 4px;
+  max-width: 480px;
   font-weight: 600;
   color: white;
   text-align: center;
